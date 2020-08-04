062590874a4fa330255c415484953f080a0e1569
Brent Benson <bwbensonjr@gmail.com>
2020-08-04 14:48:33 -0400

    Added US Covid-19 dashboard

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 RShiny applications
 
+- covid19 - Simple U.S. Covid-19 dashboard
 - polls2020 - Search and visualize 2020 Democratic primary polls
---
+++ covid19/app.R
@@ -0,0 +1,66 @@
+library(shiny)
+library(tidyverse)
+library(jsonlite)
+library(lubridate)
+
+covid_cases <- fromJSON("https://covidtracking.com/api/states/daily") %>%
+    as_tibble() %>%
+    mutate(date = ymd(date)) %>%
+    select(-hash, -dateChecked, -fips) %>%
+    pivot_longer(c(-date, -state), names_to="Category", values_to="Count")
+us_totals <- covid_cases %>%
+    group_by(date, Category) %>%
+    summarize(state = "US", Count = sum(Count, na.rm=TRUE))
+covid_cases <- covid_cases %>% bind_rows(us_totals)
+us_states <- sort(unique(covid_cases$state))
+categories <- unique(covid_cases$Category)
+
+ui <- fluidPage(
+    titlePanel("US Covid-19 Counts"),
+    a("Data from covidtracking.com", href="https://covidtracking.com/api/states/daily"),
+    sidebarLayout(
+        sidebarPanel(
+            selectizeInput("category", "Category", categories, "positive"),
+            selectizeInput("state", "State",
+                           us_states,
+                           c("MA", "NY", "WA", "NJ", "LA", "CA"),
+                           multiple=TRUE),
+            width=3
+            ),
+        mainPanel(plotOutput("covid_timeline_graph", height=470),
+                  br(),
+                  plotOutput("covid_bar_graph", height=470),
+                  br(),
+                  tableOutput("covid_table"))
+    )
+)
+server <- function(input, output) {
+    output$covid_timeline_graph <- renderPlot({
+        covid_cases %>%
+            filter(state %in% input$state) %>%
+            filter(Category == input$category) %>%
+            ggplot(aes(x=date, y=Count, color=state)) +
+            geom_line() +
+            scale_y_continuous(labels=function(x) format(x, scientific=FALSE)) +
+            theme_grey(base_size=15)
+    })
+    output$covid_bar_graph <- renderPlot({
+        covid_cases %>%
+            filter(Category == input$category) %>%
+            filter(date == max(date)) %>%
+            ggplot(aes(x=reorder(state, -Count), y=Count, fill=state, color=state)) +
+            geom_col(show.legend=FALSE) +
+            xlab("") +
+            scale_y_continuous(labels=function(x) format(x, scientific=FALSE)) +
+            theme_grey(base_size=12) +
+            theme(axis.text.x = element_text(angle = 90))
+    })
+    output$covid_table <- renderTable({
+        covid_cases %>%
+            filter(state %in% input$state) %>%
+            filter(Category == input$category) %>%
+            pivot_wider(names_from="state", values_from="Count") %>%
+            mutate(date = as.character(date))
+    })
+}
+shinyApp(ui = ui, server = server)
